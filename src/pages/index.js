a87aa6682227a9e59780ac2aab623b3338ece789
Bruno Pereira Ornelas Cabral <bruno.poc@gmail.com>
2021-04-10 17:07:23 -0300

    feat: init next project

---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Front Project Setup</title>
+      </Head>
+
+      <main>
+        Content
+      </main>
+
+      <footer>
+        Footer
+      </footer>
+    </div>
+  )
+}
