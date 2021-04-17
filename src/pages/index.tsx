9368ac6d8c58ce267e6a32c98aef80371127495b
Bruno Pereira Ornelas Cabral <bruno.poc@gmail.com>
2021-04-17 19:03:35 -0300

    feat: add prettier and styled-components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,41 @@
+import React from 'react';
 import Head from 'next/head';
+import styled from 'styled-components';
 
-export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>Front Project Setup</title>
-      </Head>
+const MainHeader = styled.header`
+    width: 100%;
+    height: 200px;
+    border-bottom: 1px solid ${props => props.theme.colors.primary};
+`;
 
-      <main>Content</main>
+const Container = styled.div`
+    max-width: 1080px;
+    width: 100%;
+    height: 100%;
+    margin: auto;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+`;
 
-      <footer>Footer</footer>
-    </div>
-  );
+export default function Home(): JSX.Element {
+    return (
+        <div>
+            <Head>
+                <title>WebComics</title>
+            </Head>
+
+            <MainHeader>
+                <Container>
+                    <div>Logo</div>
+                    <div>Menu</div>
+                </Container>
+            </MainHeader>
+
+            <main>Content</main>
+
+            <footer>Footer</footer>
+        </div>
+    );
 }
---
+++ src/styles/theme.ts
@@ -0,0 +1,9 @@
+const theme = {
+    colors: {
+        background: '#0a0a0a',
+        text: '#D8D8D8',
+        primary: '#AEAEAE',
+    },
+};
+
+export default theme;
